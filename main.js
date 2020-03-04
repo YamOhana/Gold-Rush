782f6ef45009eb0ab206eaeb6057c5c546568811
YamOhana <yamoh1993@gmail.com>
2020-03-04 10:36:47 +0200

    4rth

--- main.js
+++ main.js
@@ -52,18 +52,35 @@ $(".start").on("click", function () {
     let columSize = $(".columnInput").val()
     board.loadBoard(rawSize, columSize)
     render.renderBoard(board.getMatrix(), board.getPoints())
+    startTimer()
 
-    let time = 0
-    const timer = function(){
-    
-    
-        setTimeout(() => {
-            let timerDiv = document.getElementById("timer");
-            time++;
-            timerDiv.innerHTML = time;
-            timer();
-        }, 1000);
-    }
+    let oneMinutes = 60 * 1,
+        display = document.querySelector('#timer');
+    startTimer(oneMinutes, display);
+
+
 
 
 })
+
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+
+            alert('Game Over')
+
+
+
+        }
+    }, 1000);
+}
+
